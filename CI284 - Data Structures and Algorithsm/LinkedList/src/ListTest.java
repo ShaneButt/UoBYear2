fefe6d2b62ade981003d1e7aec32daf73774a6d0
Shane Butt <35224905+ShaneButt@users.noreply.github.com>
2018-11-11 11:45:29 +0000

    Minor edits

--- CI284 - Data Structures and Algorithsm/LinkedList/src/ListTest.java
+++ CI284 - Data Structures and Algorithsm/LinkedList/src/ListTest.java
@@ -4,10 +4,14 @@ public class ListTest {
         List list = new List(); // Construct an empty list;
 
         // Construct Ticket data to input into the Queue;
-        Ticket shane = new Ticket("Shane Butt", "Software Issue", "Michael Jordan", 1, 3);
-        Ticket jack = new Ticket("Jack Whitchurch", "Network Issue","PB J Sarnies", 2, 3);
-        Ticket marc = new Ticket("Marc Walker", "Data Issue","Fusili McPasta", 3, 2);
-        Ticket mollie = new Ticket("Mollie Applebee", "Application Crash", "Jane Doe", 4, 1);
+        Ticket shane = new Ticket("Shane Butt",
+                "Software Issue", "Michael Jordan", 1, 3);
+        Ticket jack = new Ticket("Jack Whitchurch",
+                "Network Issue","PB J Sarnies", 2, 3);
+        Ticket marc = new Ticket("Marc Walker",
+                "Data Issue","Fusili McPasta", 3, 2);
+        Ticket mollie = new Ticket("Mollie Applebee",
+                "Application Crash", "Jane Doe", 4, 1);
 
         // Construct Nodes out of the Ticket information;
         Node shaneNode = list.Add(shane, shane.getPriority());
@@ -16,7 +20,8 @@ public class ListTest {
         Node mollieNode = list.Add(mollie, mollie.getPriority());
 
         // Extra Ticket for method testing;
-        Ticket elena = new Ticket("Elena Barry","Software Issue", "Michael Jordan", 5, 1);
+        Ticket elena = new Ticket("Elena Barry",
+                "Software Issue", "Michael Jordan", 5, 1);
 
 
         // Outputs
